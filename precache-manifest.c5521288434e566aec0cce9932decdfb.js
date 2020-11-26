7b23b86bbcc9d997df783a0ab46ac544de11ae1c
jul4ajka <jul4ajka@gmail.com>
2020-11-26 09:47:59 +0200

    Updates

---
+++ precache-manifest.c5521288434e566aec0cce9932decdfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717203bf325a689ff51d8f6389528108",
+    "url": "/app-react-redux-pokemon/index.html"
+  },
+  {
+    "revision": "0d37055ae1f3a01ef1b9",
+    "url": "/app-react-redux-pokemon/static/css/main.59b78124.chunk.css"
+  },
+  {
+    "revision": "a93a1c5e326769c3b191",
+    "url": "/app-react-redux-pokemon/static/js/2.f881fe03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-react-redux-pokemon/static/js/2.f881fe03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d37055ae1f3a01ef1b9",
+    "url": "/app-react-redux-pokemon/static/js/main.d19560f4.chunk.js"
+  },
+  {
+    "revision": "61e1cbf88b46bfca3593",
+    "url": "/app-react-redux-pokemon/static/js/runtime-main.d932a634.js"
+  },
+  {
+    "revision": "30a4b88c419dc9351ccc58cf7fa9071a",
+    "url": "/app-react-redux-pokemon/static/media/pickachu.30a4b88c.jpg"
+  }
+]);
